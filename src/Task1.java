a3ad32375515ea3d4f64f97cfcf1a8e49c2bb19f
Maria <mariasaskevich@gmail.com>
2022-07-08 12:00:26 +0300

    threads

--- src/Task1.java
+++ src/Task1.java
@@ -3,18 +3,17 @@ public class Task1 {
           /*
         1. Создать три потока Т1, Т2 и ТЗ
 Реализовать выполнение поток в последовательности ТЗ -> T2 -> T1 (используя метод join) +
-2. Напишите многопоточный ограниченный буфер с использованием ReentrantLock.
-
          */
         MyThread t1 = new MyThread(1, 5);
         MyThread t2 = new MyThread(5, 20);
         MyThread t3 = new MyThread(20, 40);
-        t3.start();
-        t2.start();
-        t1.start();
+
         try {
+            t3.start();
             t3.join();
+            t2.start();
             t2.join();
+            t1.start();
             t1.join();
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
--- src/Task2.java
+++ src/Task2.java
@@ -1,34 +1,41 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Task2 {
     public static void main(String[] args) {
-        // 3. Напишите многопоточный ограниченный буфер с использованием synchronized.
-        Object monitor = new Object();
+//2. Напишите многопоточный ограниченный буфер с использованием ReentrantLock.
+        Lock lock = new ReentrantLock();
+        Condition condition = lock.newCondition();
         final String[] message = {""};
         new Thread(() -> {
             try {
-                synchronized (monitor) {
-                    while (!message[0].equals("have a nice day")) {
-                        System.out.println("good night is waiting good day");
-                        monitor.wait();
-                    }
-                    message[0] = "good night";
-                    System.out.println(message[0]);
-                    monitor.notifyAll();
+                lock.lock();
+                while (!message[0].equals("have a nice day")) {
+                    System.out.println("good night is waiting good day");
+                    condition.await();
                 }
+                message[0] = "good night";
+                System.out.println(message[0]);
+                condition.signalAll();
+                lock.unlock();
+
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         }).start();
         new Thread(() -> {
             try {
-                synchronized (monitor) {
-                    while (!message[0].equals("")) {
-                        System.out.println("good morning is waiting empty thread");
-                        monitor.wait();
-                    }
-                    message[0] = "good morning";
-                    System.out.println(message[0]);
-                    monitor.notifyAll();
+                lock.unlock();
+                while (!message[0].equals("")) {
+                    System.out.println("good morning is waiting empty thread");
+                    condition.await();
                 }
+                message[0] = "good morning";
+                System.out.println(message[0]);
+                condition.signalAll();
+                lock.unlock();
+
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -36,15 +43,16 @@ public class Task2 {
 
         new Thread(() -> {
             try {
-                synchronized (monitor) {
-                    while (!message[0].equals("good morning")) {
-                        System.out.println("have a nice day is waiting good morning");
-                        monitor.wait();
-                    }
-                    message[0] = "have a nice day";
-                    System.out.println(message[0]);
-                    monitor.notifyAll();
+                lock.lock();
+                while (!message[0].equals("good morning")) {
+                    System.out.println("have a nice day is waiting good morning");
+                    condition.await();
                 }
+                message[0] = "have a nice day";
+                System.out.println(message[0]);
+                condition.signalAll();
+                lock.unlock();
+
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
